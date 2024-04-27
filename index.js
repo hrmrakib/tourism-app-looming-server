030f96b47bfb55151b7b971a2806c00e4b471adf
hrmrakib <hrmrakibs@gmail.com>
2024-04-27 18:08:43 +0600

    add a route for set & get tourist spot data

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("helllo server");
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.dmwxvyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+    const tourismApp = client.db("tourismApp");
+    const allSpot = tourismApp.collection("allSpot");
+
+    app.get("/allspot", async (req, res) => {
+      const data = allSpot.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+    app.post("/allspot", async (req, res) => {
+      const spotInfo = req.body;
+      const result = await allSpot.insertOne(spotInfo);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
